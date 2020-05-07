7e1059f0f4c79c59972308f3b3dc53f116ddc52d
berliana123 <berlianaaliftiaifada@gmail.com>
2020-05-07 19:46:28 +0700

    test pwa

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <navbar/>
+    <b-container fluid>
+      <app-logo/>
+      <Categories/>
+      <meals/>
+      <Area/>
+      <Refs/>
+    </b-container>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import Navbar from '~/components/Navbar.vue'
+import Area from '~/components/Area.vue'
+import Categories from '~/components/Category.vue'
+import Meals from '~/components/Meals.vue'
+import Refs from '~/components/Reference.vue'
+import Footer from '~/components/footer.vue'
+
+export default {
+  components: {
+    AppLogo,
+    Navbar,
+    Area,
+    Categories,
+    Meals,
+    Refs,
+    Footer
+  }
+}
+</script>
+
+<style>
+.container-fluid {
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  text-align: none;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
